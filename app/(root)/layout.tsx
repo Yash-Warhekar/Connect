0de46ed5a208727ff6b5b5a02c052786acddb663
yash_warhekar <yash2000warhekar@gmail.com>
2025-03-24 01:24:39 +0530

    ready to deploy

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,24 @@
+import { ReactNode } from 'react';
+
+import StreamVideoProvider from '@/providers/StreamClientProvider';
+import { Metadata } from 'next';
+
+
+export const metadata: Metadata = {
+  title: "Connect",
+  description: "Video calling application",
+  icons:{
+    icon:'/icons/logo.svg'
+  }
+
+};
+
+const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
+  return (
+    <main>
+      <StreamVideoProvider>{children}</StreamVideoProvider>
+    </main>
+  );
+};
+
+export default RootLayout;
